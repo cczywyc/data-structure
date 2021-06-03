1d170ac0a1d3b99783f7db8ed27678dce9450468
cczyWyc <274230928@qq.com>
2021-06-04 00:00:08 +0800

    排序算法-希尔排序

---
+++ src/main/java/com/wangyc/algorithm/sorting/ShellSort.java
@@ -0,0 +1,31 @@
+package com.wangyc.algorithm.sorting;
+
+import java.util.Arrays;
+
+/**
+ * 希尔排序
+ * @author wangyc
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        shellSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void shellSort(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0 ; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
