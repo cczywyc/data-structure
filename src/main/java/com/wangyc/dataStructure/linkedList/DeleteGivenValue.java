8acd9e15351cd58f25aa1db84614c11234278663
cczyWyc <274230928@qq.com>
2023-02-07 00:26:20 +0800

    list: delete list node

---
+++ src/main/java/com/wangyc/dataStructure/linkedList/DeleteGivenValue.java
@@ -0,0 +1,35 @@
+package com.wangyc.dataStructure.linkedList;
+
+/**
+ * @author cczywyc
+ */
+public class DeleteGivenValue {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node deleteListNode(Node head, int num) {
+        while (head != null) {
+            if (head.value != num) {
+                break;
+            }
+            head = head.next;
+        }
+        Node pre = head;
+        Node cur = head;
+        while (cur != null) {
+            if (cur.value == num) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
