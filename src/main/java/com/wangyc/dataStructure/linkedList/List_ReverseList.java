51b394abd5d9c8571b57789b5a2fad6a9f0f0314
cczyWyc <274230928@qq.com>
2023-02-06 23:53:18 +0800

    list: reverse list

---
+++ src/main/java/com/wangyc/dataStructure/linkedList/List_ReverseList.java
@@ -0,0 +1,50 @@
+package com.wangyc.dataStructure.linkedList;
+
+/**
+ * @author cczywyc
+ */
+public class List_ReverseList {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static Node ReverseList(Node head) {
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    public static class DoubleNode {
+        public int value;
+        public DoubleNode last;
+        public DoubleNode next;
+
+        public DoubleNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static DoubleNode reverseDoubleList(DoubleNode head) {
+        DoubleNode pre = null;
+        DoubleNode next;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            head.last = next;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
